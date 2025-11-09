024659dde3d534da267daa93bcceec49747e76dc
mathstutor19 <mathstutor19@gmail.com>
2025-11-09 20:43:33 +0500

    feat:add vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        {/* Navigation */}
         <Header />
-        {/* Routes */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/menu" element={<Menu />} />
